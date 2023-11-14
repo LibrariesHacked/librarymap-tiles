24e50b0077c9d4fae942391675f52cd175ca9585
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-11-14 23:50:09 +0000

    Adding --no-tile-size-limit to tiles

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 rm -rf tiles
 mkdir tiles
-tippecanoe -r1 --no-tile-compression --output-to-directory --drop-densest-as-needed libraries data/libraries.geojson --force
+tippecanoe -r1 --no-tile-compression --output-to-directory --drop-densest-as-needed --no-tile-size-limit libraries data/libraries.geojson --force
 find . -name '*.pbf' -exec sh -c 'mv "$0" "${0%.pbf}.mvt"' {} \;
