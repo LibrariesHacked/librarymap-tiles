8be89d94826a1596ff3a9e20cdcdcaa455675717
Dave <8709533+DaveBathnes@users.noreply.github.com>
2023-11-14 22:28:26 +0000

    Adjusting zoom filtering

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 rm -rf tiles
 mkdir tiles
-tippecanoe --no-tile-compression --output-to-directory tiles data/libraries.geojson
+tippecanoe -r1 --no-tile-compression --output-to-directory libraries data/libraries.geojson
 find . -name '*.pbf' -exec sh -c 'mv "$0" "${0%.pbf}.mvt"' {} \;
